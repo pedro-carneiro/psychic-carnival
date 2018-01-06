b02034f2211b33c280795440a91ea4450590e5f4
Pedro Carneiro <carneiro.pedro@gmail.com>
2018-01-06 19:59:55 +0000

    Update wiring to include services

--- src/ToggleApi/Startup.cs
+++ src/ToggleApi/Startup.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using ToggleApi.Models;
-using ToggleApi.Models.Resources;
-using ToggleApi.Services;
-using ToggleApi.Services.Toggles;
-using AutoMapper;
-
 namespace ToggleApi
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using ToggleApi.Models.Resources;
+    using ToggleApi.Services;
+    using ToggleApi.Services.Toggles;
+    using AutoMapper;
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ToggleContext>(opt => opt.UseInMemoryDatabase("ToggleList"));
+            services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("ToggleList"));
             services.AddMvc();
             services.AddAutoMapper();
 
-            services.AddSingleton(typeof(ICreateService<ToggleResource>), typeof(ToggleCreateService));
-            services.AddSingleton(typeof(IReadService<ToggleResource>), typeof(ToggleReadService));
-            services.AddSingleton(typeof(IUpdateService<ToggleResource>), typeof(ToggleUpdateService));
-            services.AddSingleton(typeof(IDeleteService<ToggleResource>), typeof(ToggleDeleteService));
+            services.AddScoped(typeof(ICreateService<ToggleResource>), typeof(ToggleCreateService));
+            services.AddScoped(typeof(IReadService<ToggleResource>), typeof(ToggleReadService));
+            services.AddScoped(typeof(IUpdateService<ToggleResource>), typeof(ToggleUpdateService));
+            services.AddScoped(typeof(IDeleteService<ToggleResource>), typeof(ToggleDeleteService));
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMiddleware(typeof(ErrorHandlingMiddleware));
             app.UseMvc();
         }
     }
