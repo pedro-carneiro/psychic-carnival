303f08381a6f998509e9b615f54a1a0f1fd21166
Pedro Carneiro <carneiro.pedro@gmail.com>
2018-01-06 16:02:35 +0000

    Add basic project files
    This changes adds the basic project files, in order to build the toggles
    API.

---
+++ src/ToggleApi/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ToggleApi.Models;
+using ToggleApi.Models.Resources;
+using ToggleApi.Services;
+using ToggleApi.Services.Toggles;
+using AutoMapper;
+
+namespace ToggleApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ToggleContext>(opt => opt.UseInMemoryDatabase("ToggleList"));
+            services.AddMvc();
+            services.AddAutoMapper();
+
+            services.AddSingleton(typeof(ICreateService<ToggleResource>), typeof(ToggleCreateService));
+            services.AddSingleton(typeof(IReadService<ToggleResource>), typeof(ToggleReadService));
+            services.AddSingleton(typeof(IUpdateService<ToggleResource>), typeof(ToggleUpdateService));
+            services.AddSingleton(typeof(IDeleteService<ToggleResource>), typeof(ToggleDeleteService));
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
