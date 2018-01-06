8b308de12042a95eab1ee9370124673e9a7d5fdb
Pedro Carneiro <carneiro.pedro@gmail.com>
2018-01-06 19:57:24 +0000

    Add datastore context class
    This change adds the project's datastore class, that provides all
    interaction methods to read and write resources.

---
+++ src/ToggleApi/ApiDbContext.cs
@@ -0,0 +1,15 @@
+namespace ToggleApi
+{
+    using Microsoft.EntityFrameworkCore;
+    using ToggleApi.Models.Entities;
+
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Toggle> Toggles { get; set; }
+    }
+}
