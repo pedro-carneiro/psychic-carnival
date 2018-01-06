693a410262bcaab8f2df770633c196913e25d2c1
Pedro Carneiro <carneiro.pedro@gmail.com>
2018-01-06 19:56:17 +0000

    Add toggle entity
    This change adds a class to the project that represents a Toggle entity
    with an internal character. Think of it as service domain.

---
+++ src/ToggleApi/Models/Entities/Toggle.cs
@@ -0,0 +1,9 @@
+namespace ToggleApi.Models.Entities
+{
+    public class Toggle
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool DefaultValue { get; set; }
+    }
+}
