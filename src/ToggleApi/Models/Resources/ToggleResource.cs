9afea62ca5fa49c984e6d2342d93b62a54b99998
Pedro Carneiro <carneiro.pedro@gmail.com>
2018-01-06 16:03:47 +0000

    Add initial version of Toggle resource
    This change adds the initial version of the Toggle resource
    representation class. This class is serialised directly as JSON on the
    API's responses.

---
+++ src/ToggleApi/Models/Resources/ToggleResource.cs
@@ -0,0 +1,9 @@
+namespace ToggleApi.Models.Resources
+{
+    public class ToggleResource
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool DefaultValue { get; set; }
+    }
+}
